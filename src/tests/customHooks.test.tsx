86e0a593fc6d0b9aaf403e7a6343e17dd4473c68
amIAqua <sidelnikov224@gmail.com>
2021-01-25 22:07:52 +0100

    hooks tests added

--- src/tests/customHooks.test.tsx
+++ src/tests/customHooks.test.tsx
@@ -1,10 +1,50 @@
-import { renderHook } from '@testing-library/react-hooks'
 import { useContainElements } from '../utils/useContainElements'
 import { useMessages } from '../utils/useMessages'
+import { renderHook } from '@testing-library/react-hooks'
+import { store } from '../store'
+import { Provider } from 'react-redux'
 
 describe('useContainElements', () => {
+  let globalResult: any
+
+  beforeEach(() => {
+    const { result } = renderHook(() => useContainElements(), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+    })
+    globalResult = result
+  })
   it('should be defined', () => {
     expect(useContainElements).toBeDefined()
+  })
+
+  it('all cases list should be defined', () => {
+    expect(globalResult.current.cases).toBeDefined()
+  })
+
+  it('should return all cases equal to store value', () => {
+    expect(globalResult.current.cases).toEqual(
+      store.getState().caseReducer.allCasesList
+    )
+  })
+
+  it('cases in progress list should be defined', () => {
+    expect(globalResult.current.inProgress).toBeDefined()
+  })
+
+  it('should return cases in progress list equal to store value', () => {
+    expect(globalResult.current.inProgress).toEqual(
+      store.getState().caseInProgressReducer.allCasesInProgressList
+    )
+  })
+
+  it('completed cases list should be defined', () => {
+    expect(globalResult.current.completed).toBeDefined()
+  })
+
+  it('completed cases list equal to store value', () => {
+    expect(globalResult.current.inProgress).toEqual(
+      store.getState().caseInProgressReducer.completedCases
+    )
   })
 })
 
